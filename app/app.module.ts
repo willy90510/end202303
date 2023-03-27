775cfafd69eb60fc3089da5930d789c6841fd489
willy90510 <88758212+willy90510@users.noreply.github.com>
2023-03-27 15:35:24 +0800

    4 files modified, 1 added

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { MatTabsModule } from '@angular/material/tabs'; 
 
 import { AppComponent } from './app.component';
-import {MagicComponent} from './magic.component';
+import { MagicComponent } from './magic.component';
+import { BrowserAnimationsModule } from 
+'@angular/platform-browser/animations';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, MagicComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule,MatTabsModule,
+    BrowserAnimationsModule],
+  declarations: [AppComponent, MagicComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-
- }
+export class AppModule {}
